b520dd131df3dbfb3c97db18654bdc7e5046bffe
Aaron Granick <aaron.granick@okta.com>
2022-02-05 02:04:22 -0800

    authjs beta

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,7 +14,7 @@ echo "Installing BETA VERSION"
 
 npm config set strict-ssl false
 
-yarn add -DW --no-lockfile https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js-6.0.0-g94db762.tgz
+yarn add -DW --no-lockfile https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js-6.0.0-g575ab60.tgz
 
 echo "BETA VERSION installed"
 # End BETA VERSION
